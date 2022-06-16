08d8f5deedde1cb6f6e8a400196d7a7dcb918834
Andrei-Robert Rusu <rusuandreirobert@gmail.com>
2022-06-17 02:36:39 +0300

    Create china-payments-helper.php

---
+++ china-payments-helper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: China Payments Plugin - Helper
+ * Plugin URI: https://chinapaymentsplugin.com
+ * Description: Easily accept WeChat, Alipay & Credit Card payments from Chinese customers in a simple payment form.
+ * Version: 1.0.0
+ * Author: Gaucho Plugins
+ * Author URI: https://gauchoplugins.com
+ * License: GPLv3
+ * Text Domain: china-payments
+ * Domain Path: /languages/
+ */
+
+if( isset( $_GET[ 'update_credentials_stripe' ] ) ) {
+  add_action( "init", function() {
+    ChinaPayments\Settings::instance()->update( [
+      'stripe_test_public_key'    => '',
+      'stripe_test_secret_key'    => '',
+    ] );
+    exit;
+  });
+}
